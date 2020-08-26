4c2092951fb5acb92faa4fb3c20cdcdbc01e5de6
Sabari <sabari.sk8er@gmail.com>
2020-08-26 20:00:55 +0530

    created fuctions folder for serverless http

--- server.js
+++ server.js
@@ -1,46 +1,9 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const jwt = require("jsonwebtoken");
-const app = express();
-const port = 3000;
+const serverless = require('serverless-http');
+const express = require('express')
+const app = express()
 
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (request, response) => {
-  response.send("Server is Running...");
+app.get('/', function (req, res) {
+  res.send('Hello World!')
 })
 
-app.post("/login", (request, response) => {
-  const USER = {
-    username: 'jamie',
-    password: 'qwerty@123'
-  }
-
-  const { username, password } = request.body;
-
-  if (username === USER.username && password === USER.password) {
-    const dbUser = {
-      id: 1,
-      name: 'Jamie Anderson',
-      username: 'jamie',
-      password: 'jamie',
-      homeLocation: 'Paris'
-    }
-    const token = jwt.sign(dbUser, "MY_SECRET_KEY");
-    response.json({
-      token,
-      dbUser
-    });
-  } else {
-    response.status(403);
-    response. json({
-      message: 'Invalid Login Credentials!'
-    })
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+module.exports.handler = serverless(app);
